00063ba2c664e599ac829ba36d7d4c738e8b1ee7
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-10-28 12:05:39 -0500

    Add files via upload

--- src/pylotl.py
+++ src/pylotl.py
@@ -1,6 +1,7 @@
+import argparse
 import re
 import requests
-import time
+import threading
 import urllib.parse
 import warnings
 from bs4 import BeautifulSoup
@@ -10,8 +11,28 @@ CYAN = "\033[1;36m"
 GREEN = "\033[0;32m"
 RED = "\033[1;31m"
 
-def pylotl(website, delay = 0):
+def aggresive(host):
+    global data
+
+    try:
+        my_session = requests.Session() 
+        my_request = my_session.get(host, verify = False, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}, timeout = 10)
+        if my_request.status_code == 200:
+            data += f"{my_request.text}\n"
+
+    except:
+        pass
+
+def pylotl():
+    global data
+    data = ""
+
     clear()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-host", type = str, required = True)
+    args = parser.parse_args()
+    website = args.host
     
     website = website.rstrip("/")
     warnings.filterwarnings("ignore")
@@ -27,80 +48,86 @@ def pylotl(website, delay = 0):
             visit_now += 1
             visited = list(dict.fromkeys(visited[:]))
             print(f"{CYAN}visiting: {GREEN}{visited[visit_now]}", flush = True)
-            time.sleep(delay)
+            my_thread = threading.Thread(target=aggresive, args = (visited[visit_now],))
+            thread_list.append(my_thread)
+            my_thread.start()
+
+            if start:
+                my_thread.join()
+                thread_list = []
+                start = False
+
+            links = []
+           
+            soup = BeautifulSoup(data, "html.parser")
+
             try:
-                my_session = requests.Session() 
-                my_request = my_session.get(visited[visit_now], verify = False, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}, timeout = 10)
-                if my_request.status_code == 200:
-                    data = my_request.text
+                new_links = soup.find_all("a")
+                for link in new_links:
+                    if link.get("href") is not None:
+                        links.append(link.get("href"))
 
             except:
                 pass
 
-            if data:
-                links = []
-               
-                soup = BeautifulSoup(data, "html.parser")
+            try:
+                soup.findAll("img")
+                images = soup.find_all("img")
+                for image in images:
+                    if image["src"] is not None:
+                        links.append(image["src"])
 
-                try:
-                    new_links = soup.find_all("a")
-                    for link in new_links:
-                        if link.get("href") is not None:
-                            links.append(link.get("href"))
+            except:
+                pass
 
-                except:
-                    pass
+            try:
+                new_links = soup.find_all("link")
+                for link in new_links:
+                    if link.get("href") is not None:
+                        links.append(link.get("href"))
+                   
+                    if link.get("imagesrcset") is not None:
+                        for i in link.get("imagesrcset").split(","):
+                            links.append(i.strip())
 
-                try:
-                    soup.findAll("img")
-                    images = soup.find_all("img")
-                    for image in images:
-                        if image["src"] is not None:
-                            links.append(image["src"])
+            except:
+                pass
+           
+            links = list(dict.fromkeys(links[:]))
+           
+            for path in links:
+                if re.search("^[a-zA-Z0-9]", path.lstrip("/")) and not re.search("script|data:", path):
+                    if path.startswith("/"):
+                        visited.append(website + path)
 
-                except:
-                    pass
+                    elif path.startswith("http://") or path.startswith("https://"):
+                        if urllib.parse.urlparse(website).netloc in urllib.parse.urlparse(path).netloc:
+                            visited.append(path)
 
-                try:
-                    new_links = soup.find_all("link")
-                    for link in new_links:
-                        if link.get("href") is not None:
-                            links.append(link.get("href"))
-                       
-                        if link.get("imagesrcset") is not None:
-                            for i in link.get("imagesrcset").split(","):
-                                links.append(i.strip())
+                    else:
+                        visited.append(website + "/" + path)
 
-                except:
-                    pass
-               
-                links = list(dict.fromkeys(links[:]))
-               
-                for path in links:
+            scripts = soup.find_all("script")
+            for script in scripts:
+                if script.get("src") is not None:
+                    path = script.get("src")
                     if re.search("^[a-zA-Z0-9]", path.lstrip("/")) and not re.search("script|data:", path):
                         if path.startswith("/"):
                             visited.append(website + path)
-
-                        elif path.startswith("http://") or path.startswith("https://"):
-                            if urllib.parse.urlparse(website).netloc in urllib.parse.urlparse(path).netloc:
-                                visited.append(path)
-
+ 
+                        elif path.startswith("http://") or path.startswith("https://") or path.startswith("ftp://"):
+                            visited.append(path)
+ 
                         else:
                             visited.append(website + "/" + path)
 
-                scripts = soup.find_all("script")
-                for script in scripts:
-                    if script.get("src") is not None:
-                        path = script.get("src")
-                        if re.search("^[a-zA-Z0-9]", path.lstrip("/")) and not re.search("script|data:", path):
-                            if path.startswith("/"):
-                                visited.append(website + path)
-     
-                            elif path.startswith("http://") or path.startswith("https://") or path.startswith("ftp://"):
-                                visited.append(path)
-     
-                            else:
-                                visited.append(website + "/" + path)
+                data_list = []
+
+            if len(thread_list) % 8 == 0:
+                for thread in thread_list:
+                    thread.join()
+
+                thread_list = []
  
         except IndexError:
             break
@@ -108,4 +135,12 @@ def pylotl(website, delay = 0):
         except:
             pass
  
-    return visited
+    with open("links.txt", "a") as file:
+        for link in visited:
+            if urllib.parse.urlparse(website).netloc in link:
+                file.write(f"{link}\n")
+
+    print(f"{RED}DONE!")
+
+if __name__ == "__main__":
+    pylotl()
