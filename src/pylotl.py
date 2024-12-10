fcc3228d993bc611afcb9bb0a53e08271462cbbb
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-12-10 11:59:43 -0600

    Add files via upload

--- src/pylotl.py
+++ src/pylotl.py
@@ -349,4 +349,4 @@ def main_html():
 if __name__ == "__main__":
     clear()
     my_thread = threading.Thread(target = automatic_index).start()
-    app.run(debug=False, port=5000)
+    app.run(debug=False, host="0.0.0.0")
